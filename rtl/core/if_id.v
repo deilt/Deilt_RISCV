32f4ca30560eff5cd0e292c95b2cdfac6f0534fb
deilt <1256572384@qq.com>
2023-03-14 20:26:52 +0800

    modify defines directory

--- rtl/core/if_id.v
+++ rtl/core/if_id.v
@@ -18,7 +18,7 @@
 // 2023-03-09   Deilt           1.0                     Original
 //  
 // *********************************************************************************
-`include "defines.v"
+`include "../defines/defines.v"
 module if_id(
     input                   clk         ,
     input                   rstn        ,
