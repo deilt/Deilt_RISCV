8c52fcfa5cf8011d93ed4044371c3f78e2db93a1
deilt <1256572384@qq.com>
2023-03-23 21:14:02 +0800

    backup

---
+++ rtl_histort/rtl_20230323_prdct_j_b_u_inst/core/ex.v
@@ -0,0 +1,306 @@
+// *********************************************************************************
+// Project Name : Deilt_RISCV
+// File Name    : ex.v
+// Module Name  : ex
+// Author       : Deilt
+// Email        : cjdeilt@qq.com
+// Website      : https://github.com/deilt/Deilt_RISC
+// Create Time  : 
+// Called By    :
+// Description  : execution
+// License      : Apache License 2.0
+//
+//
+// *********************************************************************************
+// Modification History:
+// Date         Auther          Version                 Description
+// -----------------------------------------------------------------------
+// 2023-03-10   Deilt           1.0                     Original
+// 2023-03-17   Deilt           1.1 
+// *********************************************************************************
+`include "../defines/defines.v"
+module ex(
+    input                           clk         ,
+    input                           rstn        ,
+    //from id_ex
+    input[`InstBus]                 inst_i      ,
+    input[`InstAddrBus]             instaddr_i  ,
+    input[`RegBus]                  rs1_data_i  ,//add
+    input[`RegBus]                  rs2_data_i  ,//add
+
+    input[`RegBus]                  op1_i       ,
+    input[`RegBus]                  op2_i       ,
+    input                           regs_wen_i  ,
+    input[`RegAddrBus]              rd_addr_i   ,
+    //to ex_mem
+    output[`InstBus]                inst_o      ,
+    output[`InstAddrBus]            instaddr_o  ,
+
+    output                          cs_o        ,
+    output                          mem_we_o    ,
+    output[`MemUnit-1:0]            mem_wem_o   ,  
+    output[`MemBus]                 mem_din     ,
+    output[`MemAddrBus]             mem_addr_o  ,
+
+    output                          regs_wen_o  ,
+    output[`RegAddrBus]             rd_addr_o   ,
+    output[`RegBus]                 rd_data_o   ,
+    //to ctrl
+    output                          ex_hold_flag_o ,//modify
+
+    output                          ex_jump_en_o    ,//add
+    output[`InstAddrBus]            ex_jump_base_o  ,//add
+    output[`InstAddrBus]            ex_jump_ofst_o  //add
+    
+
+);
+    reg [`InstBus]              inst_o;
+    reg [`InstAddrBus]          instaddr_o;
+    reg                         regs_wen_o;
+    reg [`RegAddrBus]           rd_addr_o;
+    reg                         ex_hold_flag_o;
+    reg                         ex_jump_en_o;  
+    reg [`InstAddrBus]          ex_jump_base_o;
+    reg [`InstAddrBus]          ex_jump_ofst_o;
+
+    wire [6:0]  opcode = inst_i[6:0];
+    wire [2:0]  funct3 = inst_i[14:12];
+    wire [6:0]  funct7 = inst_i[31:25];
+    wire [4:0]  shamt  = inst_i[24:20];
+    wire [31:0] sign_expd_imm = {{20{inst_i[31]}},inst_i[31:20]};
+    wire [31:0] sign_expd_binst_imm = {{20{inst_i[31]}},inst_i[7],inst_i[30:25],inst_i[11:8],1'b0};
+
+    reg [`RegBus]               rd_data;
+    wire [`RegBus]              op1_add_op2;
+    wire [`RegBus]              op1_sub_op2;
+    wire [`RegBus]              op1_or_op2;
+    wire [`RegBus]              op1_xor_op2;
+    wire [`RegBus]              op1_and_op2;
+
+
+    wire                        op1_be_op2_unsigned;
+    wire                        op1_be_op2_signed;
+    wire [`RegBus]              sra_arith;
+    //wire [`RegBus]              sri_shift;
+    wire [`RegBus]              sr_shift;
+    //wire [`RegBus]              sri_shift_mask;
+    wire [`RegBus]              sr_shift_mask;
+
+    wire [`RegBus]              sl_shift;
+
+    
+    //rd_data_o
+    assign rd_data_o = rd_data;
+
+    //----------------------------------------
+    //add 
+    //In order to save resources the adder here is shared
+    assign op1_add_op2 = op1_i + op2_i;
+    //-----------------------------------------
+
+    //----------------------------------------
+    //sub
+    //In order to save resources the sub here is shared
+    assign op1_sub_op2 = op1_i - op2_i;
+    //-----------------------------------------
+
+    //----------------------------------------
+    //or
+    //In order to save resources the or here is shared
+    assign op1_or_op2 = op1_i | op2_i;
+    //-----------------------------------------
+
+    //----------------------------------------
+    //xor
+    //In order to save resources the xor here is shared
+    assign op1_xor_op2 = op1_i ^ op2_i;
+    //-----------------------------------------
+
+    //----------------------------------------
+    //and
+    //In order to save resources the and here is shared
+    assign op1_and_op2 = op1_i & op2_i;
+    //-----------------------------------------
+
+    //-----------------------------------------
+    //compare logic
+    //unsigned 
+    assign op1_be_op2_unsigned = op1_i >= op2_i;
+    assign op1_eq_op2          =  (op1_i == op2_i);
+    //signed
+    assign op1_be_op2_signed   = $signed(op1_i) >= $signed(op2_i);
+
+    //----------------------------------------
+
+    //-----------------------------------------
+    //shift right arith imm
+    `ifndef SRA_NOT
+        //srai_logi = (op1_i >> shamt) | (({32{op1_i[31]}}) & ~((32'hffffffff) >> shamt));
+        assign sra_arith = (sr_shift) | (({32{op1_i[31]}}) & ~(sr_shift_mask));
+    `endif  
+
+    //assign sri_shift        = op1_i >> inst_i[24:20]; //op1_i >> shamt
+    assign sr_shift         = op1_i >> op2_i[4:0];
+    //assign sri_shift_mask   = 32'hffffffff >> inst_i[24:20];
+    assign sr_shift_mask    = 32'hffffffff >> op2_i[4:0];
+
+    //----------------------------------------
+
+    //-----------------------------------------
+    //shift left logi imm
+    assign sl_shift         = op1_i << op2_i[4:0];
+    //----------------------------------------
+    //ex
+    always @(*)begin
+        inst_o =inst_i;
+        instaddr_o = instaddr_i;
+        regs_wen_o = regs_wen_i;
+        rd_addr_o  = rd_addr_i;
+        rd_data = `ZeroReg;
+
+        ex_hold_flag_o = `HoldDisable;
+        ex_jump_en_o   = `JumpDisable;
+        ex_jump_base_o = `CpuResetAddr;
+        ex_jump_ofst_o = `ZeroWord;
+        
+        case(opcode)
+            `INST_TYPE_I:begin
+                case(funct3)
+                    `INST_ADDI:begin
+                        rd_data = op1_add_op2;
+                    end
+                    `INST_ORI:begin
+                        rd_data = op1_or_op2;//op1_i | op2_i;
+                    end
+                    `INST_XORI:begin
+                        rd_data = op1_xor_op2;
+                    end
+                    `INST_ANDI:begin
+                        rd_data = op1_and_op2;
+                    end
+                    `INST_SLTI:begin
+                        rd_data = {32{(~op1_be_op2_signed)}} & 32'h1;
+                    end
+                    `INST_SLTIU:begin
+                        rd_data = {32{(~op1_be_op2_unsigned)}} & 32'h1;
+                    end
+                    `INST_SLLI:begin
+                        rd_data = sl_shift;//op1_i << shamt; 
+                    end
+                    `INST_SRLI,`INST_SRAI:begin
+                        if(funct7 == 7'h0)begin //SRLI
+                            rd_data = sr_shift;//op1_i >> shamt
+                        end
+                        else begin
+                            //rd_data = (op1_i >> shamt) | (({32{op1_i[31]}}) & ~((32'hffffffff) >> shamt));
+                            rd_data = sra_arith;
+                        end
+                    end
+                    default:begin
+                        rd_data = `ZeroWord;
+                    end
+                endcase
+            end
+            `INST_TYPE_R_M:begin
+                case(funct3)
+                    `INST_ADD_SUB:begin
+                        if(funct7 == 7'b0000000)begin//add
+                            rd_data = op1_add_op2;
+                        end
+                        else begin//sub
+                            rd_data = op1_sub_op2;
+                        end
+                    end
+                    `INST_SLL:begin
+                        rd_data = sl_shift;//op1_i << op2_i[4:0]
+                    end
+                    `INST_SLT:begin
+                        rd_data = {32{(~op1_be_op2_signed)}} & 32'h1;
+                    end
+                    `INST_SLTU:begin
+                        rd_data = {32{(~op1_be_op2_unsigned)}} & 32'h1;
+                    end
+                    `INST_XOR:begin
+                        rd_data = op1_xor_op2;
+                    end
+                    `INST_SRL_SRA:begin
+                        if(funct7 == 7'b0000000)begin //ral
+                            rd_data = sr_shift;
+                        end
+                        else begin //sra
+                            rd_data = sra_arith;
+                        end
+                    end
+                    `INST_OR:begin
+                        rd_data = op1_or_op2;
+                    end
+                    `INST_AND:begin
+                        rd_data = op1_and_op2;
+                    end
+                endcase 
+            end
+            `INST_JAL:begin
+                rd_data = op1_add_op2;//pc+4
+                ex_jump_en_o   = `JumpEnable;
+                ex_jump_base_o = instaddr_i;
+                ex_jump_ofst_o = {{12{inst_i[31]}},inst_i[19:12],inst_i[20],inst_i[30:21],1'b0};//sign_imm
+            end
+            `INST_JALR:begin
+                rd_data = op1_add_op2;//pc+4
+                ex_jump_en_o   = `JumpEnable;
+                ex_jump_base_o = rs1_data_i;
+                ex_jump_ofst_o = sign_expd_imm;//sign_imm
+            end
+            `INST_TYPE_B:begin
+                case(funct3)
+                    `INST_BEQ:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = op1_eq_op2;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                    `INST_BNE:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = !op1_eq_op2;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                    `INST_BLT:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = !op1_be_op2_signed;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                    `INST_BGE:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = op1_be_op2_signed;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                    `INST_BLTU:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = !op1_be_op2_unsigned;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                    `INST_BGEU:begin
+                        ex_hold_flag_o = `HoldDisable;
+                        ex_jump_en_o   = op1_be_op2_unsigned;
+                        ex_jump_base_o = instaddr_i;
+                        ex_jump_ofst_o = sign_expd_binst_imm;
+                    end
+                endcase
+            end
+            `INST_TYPE_LUI:begin
+                rd_data = op1_add_op2;
+            end
+            `INST_TYPE_AUIPC:begin
+                rd_data = op1_add_op2;
+            end
+            default:begin
+                rd_data = `ZeroWord;
+            end
+        endcase
+    end
+
+endmodule
---
+++ rtl_histort/rtl_20230323_prdct_j_b_u_inst/core/riscv_core.v
@@ -0,0 +1,375 @@
+// *********************************************************************************
+// Project Name : Deilt_RISCV
+// File Name    : riscv_core.c
+// Module Name  : riscv_core
+// Author       : Deilt
+// Email        : cjdeilt@qq.com
+// Website      : https://github.com/deilt/Deilt_RISC
+// Create Time  : 
+// Called By    :
+// Description  :
+// License      : Apache License 2.0
+//
+//
+// *********************************************************************************
+// Modification History:
+// Date         Auther          Version                 Description
+// -----------------------------------------------------------------------
+// 2023-03-13   Deilt           1.0                     Original
+//  
+// *********************************************************************************
+`include "../defines/defines.v"
+module riscv_core(
+    input                           clk ,
+    input                           rstn
+);
+    //pc
+    wire [`InstAddrBus]         pc_o;
+    //rom
+    wire [`InstBus]             rom_data_o;
+    //if_id
+    wire [`InstBus]             if_id_inst_o;
+    wire [`InstAddrBus]         if_id_instaddr_o;
+    //id
+    wire                        rs1_read_o;
+    wire                        rs2_read_o;
+    wire [`RegAddrBus]          rs1_addr_o;
+    wire [`RegAddrBus]          rs2_addr_o;
+    wire [`InstBus]             id_inst_o;
+    wire [`InstAddrBus]         id_instaddr_o;
+    wire [`RegBus]              id_op1_o;
+    wire [`RegBus]              id_op2_o;
+    wire                        id_regs_wen_o;
+    wire [`RegAddrBus]          id_rd_addr_o;
+    //reg
+    wire [`RegBus]              rs1_data_o;
+    wire [`RegBus]              rs2_data_o;
+    //id_ex
+    wire [`InstBus]             id_ex_inst_o;
+    wire [`InstAddrBus]         id_ex_instaddr_o;
+    wire [`RegBus]              id_ex_op1_o;
+    wire [`RegBus]              id_ex_op2_o;
+    wire                        id_ex_regs_wen_o;
+    wire [`RegAddrBus]          id_ex_rd_addr_o;
+    wire [`RegBus]              id_ex_rs1_data_o ;
+    wire [`RegBus]              id_ex_rs2_data_o ;
+    wire                        id_ex_jump_en_o;
+    //ex
+    wire [`InstBus]             ex_inst_o;
+    wire [`InstAddrBus]         ex_instaddr_o;
+
+    wire                        cs_o;        
+    wire                        mem_we_o;    
+    wire [`MemUnit-1:0]         mem_wem_o;     
+    wire [`MemBus]              mem_din;     
+    wire [`MemAddrBus]          mem_addr_o;  
+
+    wire                        ex_regs_wen_o;
+    wire [`RegAddrBus]          ex_rd_addr_o;
+    wire [`RegBus]              ex_rd_data_o;
+    wire                        ex_hold_flag_o;
+
+    wire                          ex_jump_en_o;
+    wire[`InstAddrBus]            ex_jump_base_o;
+    wire[`InstAddrBus]            ex_jump_ofst_o;
+    //ex_mem
+    wire [`InstBus]             ex_mem_inst_o;
+    wire [`InstAddrBus]         ex_mem_instaddr_o;
+
+    wire                        ex_mem_cs_o;        
+    wire                        ex_mem_we_o;    
+    wire [`MemUnit-1:0]         ex_mem_wem_o;     
+    wire [`MemBus]              ex_mem_dout;     
+    wire [`MemAddrBus]          ex_mem_addr_o; 
+
+    wire                        ex_mem_regs_wen_o;
+    wire [`RegAddrBus]          ex_mem_rd_addr_o;
+    wire [`RegBus]              ex_mem_rd_data_o;
+
+    //mem
+    wire [`InstBus]             mem_inst_o;
+    wire [`InstAddrBus]         mem_instaddr_o;
+    wire                        mem_regs_wen_o;
+    wire [`RegAddrBus]          mem_rd_addr_o;
+    wire [`RegBus]              mem_rd_data_o;
+    
+    //ram
+    wire [`MemBus]              mem_data_o;
+
+    //mem_wb
+    wire [`InstBus]             mem_wb_inst_o;
+    wire [`InstAddrBus]         mem_wb_instaddr_o;
+    wire                        mem_wb_regs_wen_o;
+    wire [`RegAddrBus]          mem_wb_rd_addr_o;
+    wire [`RegBus]              mem_wb_rd_data_o;
+
+    //wb
+    wire                        wb_regs_wen_o;
+    wire [`RegAddrBus]          wb_rd_addr_o;
+    wire [`RegBus]              wb_rd_data_o;
+    //ctrl
+    wire [4:0]                  hold_en_o;
+    wire                        prd_fail;
+    //prd
+    wire                        prd_jump_en_o;
+    wire [`InstAddrBus]         prd_jump_base_o;
+    wire [`InstAddrBus]         prd_jump_ofset_o;
+
+
+    //prd
+    prdct u_prdct(
+        .clk              (clk                  ),      
+        .rstn             (rstn                 ),
+        .inst_i           (if_id_inst_o         ),    
+        .instaddr_i       (if_id_instaddr_o     ),
+        .prd_jump_en_o    (prd_jump_en_o        ),
+        .prd_jump_base_o  (prd_jump_base_o      ),
+        .prd_jump_ofset_o (prd_jump_ofset_o     ),
+        .rs1_data_i       (rs1_data_o           ),
+        .rs2_data_i       (rs2_data_o           ),
+        .rs1_addr_i       (rs1_addr_o           ),
+        .rs2_addr_i       (rs2_addr_o           ),
+        .rs1_read_i       (rs1_read_o           ),
+        .rs2_read_i       (rs2_read_o           ),
+        .ex_wen_i         (ex_regs_wen_o        ),
+        .ex_wr_addr_i     (ex_rd_addr_o         ),
+        .ex_wr_data_i     (ex_rd_data_o         ),
+        .mem_wen_i        (mem_regs_wen_o       ),
+        .mem_wr_addr_i    (mem_rd_addr_o        ),
+        .mem_wr_data_i    (mem_rd_data_o        ) 
+    );
+
+    //ctrl
+    ctrl u_ctrl(
+        .clk            (clk            ),
+        .rstn           (rstn           ),
+
+        .ex_hold_flag_i (ex_hold_flag_o ),
+        .ex_jump_en_i   (ex_jump_en_o   ),
+        .ex_jump_base_i (ex_jump_base_o ),
+        .ex_jump_ofst_i (ex_jump_ofst_o ),
+        .prd_jump_en_i  (prd_jump_en_o  ),
+        .prd_fail       (prd_fail       ),
+        .id_ex_jump_en_i(id_ex_jump_en_o),
+        .hold_en_o      (hold_en_o      )
+    );
+
+    //pc
+    pc u_pc(
+        .clk                (clk                ),
+        .rstn               (rstn               ),
+        .hold_en_i          (hold_en_o          ),
+        .prd_fail           (prd_fail           ),
+
+        .ex_instaddr_i      (ex_instaddr_o      ),
+        .ex_jump_en_i       (ex_jump_en_o       ),
+        .ex_jump_base_i     (ex_jump_base_o     ),
+        .ex_jump_ofset_i    (ex_jump_ofst_o     ),
+        .prd_jump_en_i      (prd_jump_en_o      ), 
+        .prd_jump_base_i    (prd_jump_base_o    ),
+        .prd_jump_ofset_i   (prd_jump_ofset_o   ),
+        .pc_o               (pc_o               )
+    );
+
+    //rom
+    rom u_rom(
+        .clk            (clk        ),
+        .instaddr       (pc_o         ),
+        .data_in        (`ZeroWord  ),
+        .cs             (1'b1       ),
+        .we             (1'b0       ),
+        .wem            (4'h0       ),
+        .data_o         (rom_data_o )        
+    );
+
+    //if_id
+    if_id u_if_id(
+        .clk            (clk             ),
+        .rstn           (rstn            ),
+        .inst_i         (rom_data_o      ),
+        .instaddr_i     (pc_o              ),
+        .inst_o         (if_id_inst_o    ),
+        .instaddr_o     (if_id_instaddr_o),
+        .hold_en_i      (hold_en_o       )  
+    );
+
+    //id
+    id u_id(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .inst_i         (if_id_inst_o       ),
+        .instaddr_i     (if_id_instaddr_o   ),
+        .rs1_data_i     (rs1_data_o         ),
+        .rs2_data_i     (rs2_data_o         ),
+        .rs1_addr_o     (rs1_addr_o         ),
+        .rs2_addr_o     (rs2_addr_o         ),
+        .rs1_read_o     (rs1_read_o         ),
+        .rs2_read_o     (rs2_read_o         ),
+        .inst_o         (id_inst_o          ),
+        .instaddr_o     (id_instaddr_o      ),
+        .op1_o          (id_op1_o           ),
+        .op2_o          (id_op2_o           ),
+        .regs_wen_o     (id_regs_wen_o      ),
+        .rd_addr_o      (id_rd_addr_o       ),
+        .ex_wen_i       (ex_regs_wen_o      ),
+        .ex_wr_addr_i   (ex_rd_addr_o       ),
+        .ex_wr_data_i   (ex_rd_data_o       ),
+        .mem_wen_i      (mem_regs_wen_o     ),
+        .mem_wr_addr_i  (mem_rd_addr_o      ),
+        .mem_wr_data_i  (mem_rd_data_o      )        
+    );
+    //reg
+    regfile u_regfile(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .rs1_addr_i     (rs1_addr_o         ),
+        .rs2_addr_i     (rs2_addr_o         ),
+        .rs1_read_i     (rs1_read_o         ),
+        .rs2_read_i     (rs2_read_o         ),
+        .rs1_data_o     (rs1_data_o         ),
+        .rs2_data_o     (rs2_data_o         ),
+        .wen            (wb_regs_wen_o      ),
+        .wr_addr_i      (wb_rd_addr_o       ),
+        .wr_data_i      (wb_rd_data_o       )
+    );
+
+    //id_ex
+    
+    id_ex u_id_ex(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .inst_i         (id_inst_o          ),
+        .instaddr_i     (id_instaddr_o      ),
+        .op1_i          (id_op1_o           ),
+        .op2_i          (id_op2_o           ),
+        .regs_wen_i     (id_regs_wen_o      ),
+        .rd_addr_i      (id_rd_addr_o       ),
+        .rs1_data_i     (rs1_data_o         ),
+        .rs2_data_i     (rs2_data_o         ),
+        .inst_o         (id_ex_inst_o       ),
+        .instaddr_o     (id_ex_instaddr_o   ),
+        .op1_o          (id_ex_op1_o        ),
+        .op2_o          (id_ex_op2_o        ),
+        .regs_wen_o     (id_ex_regs_wen_o   ),
+        .rd_addr_o      (id_ex_rd_addr_o    ),
+        .rs1_data_o     (id_ex_rs1_data_o   ),
+        .rs2_data_o     (id_ex_rs2_data_o   ),
+        .prd_jump_en_o  (id_ex_jump_en_o    ),
+        .prd_jump_en_i  (prd_jump_en_o      ),
+        .hold_en_i      (hold_en_o          )        
+    );
+
+    //ex
+    ex u_ex(
+        .clk            (clk                ), 
+        .rstn           (rstn               ), 
+        .inst_i         (id_ex_inst_o       ), 
+        .instaddr_i     (id_ex_instaddr_o   ),
+        .rs1_data_i     (id_ex_rs1_data_o   ),
+        .rs2_data_i     (id_ex_rs2_data_o   ),
+        .op1_i          (id_ex_op1_o        ), 
+        .op2_i          (id_ex_op2_o        ), 
+        .regs_wen_i     (id_ex_regs_wen_o   ), 
+        .rd_addr_i      (id_ex_rd_addr_o    ), 
+        .inst_o         (ex_inst_o          ), 
+        .instaddr_o     (ex_instaddr_o      ), 
+        .cs_o           (cs_o               ), 
+        .mem_we_o       (mem_we_o           ), 
+        .mem_wem_o      (mem_wem_o          ), 
+        .mem_din        (mem_din            ), 
+        .mem_addr_o     (mem_addr_o         ), 
+        .regs_wen_o     (ex_regs_wen_o      ), 
+        .rd_addr_o      (ex_rd_addr_o       ), 
+        .rd_data_o      (ex_rd_data_o       ),
+        .ex_hold_flag_o (ex_hold_flag_o     ),
+        .ex_jump_en_o   (ex_jump_en_o       ),
+        .ex_jump_base_o (ex_jump_base_o     ),
+        .ex_jump_ofst_o (ex_jump_ofst_o     )
+    );
+
+    //ex_mem
+    ex_mem u_ex_mem(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .inst_i         (ex_inst_o          ),
+        .instaddr_i     (ex_instaddr_o      ),
+        .cs_i           (cs_o               ),
+        .mem_we_i       (mem_we_o           ),
+        .mem_wem_i      (mem_wem_o          ),
+        .mem_din        (mem_din            ),
+        .mem_addr_i     (mem_addr_o         ),
+        .regs_wen_i     (ex_regs_wen_o      ),
+        .rd_addr_i      (ex_rd_addr_o       ),
+        .rd_data_i      (ex_rd_data_o       ),
+        .inst_o         (ex_mem_inst_o      ),
+        .instaddr_o     (ex_mem_instaddr_o  ),
+        .cs_o           (ex_mem_cs_o        ),
+        .mem_we_o       (ex_mem_we_o        ),
+        .mem_wem_o      (ex_mem_wem_o       ),
+        .mem_dout       (ex_mem_dout        ),
+        .mem_addr_o     (ex_mem_addr_o      ),
+        .regs_wen_o     (ex_mem_regs_wen_o  ),
+        .rd_addr_o      (ex_mem_rd_addr_o   ),
+        .rd_data_o      (ex_mem_rd_data_o   ),
+        .hold_en_i      (hold_en_o          )
+    );
+
+    //mem
+    mem u_mem(
+        .clk            (clk                ), 
+        .rstn           (rstn               ), 
+        .inst_i         (ex_mem_inst_o      ), 
+        .instaddr_i     (ex_mem_instaddr_o  ), 
+        .regs_wen_i     (ex_mem_regs_wen_o  ), 
+        .rd_addr_i      (ex_mem_rd_addr_o   ), 
+        .rd_data_i      (ex_mem_rd_data_o   ), 
+        .mem_data_i     (mem_data_o         ), 
+        .inst_o         (mem_inst_o         ), 
+        .instaddr_o     (mem_instaddr_o     ), 
+        .regs_wen_o     (mem_regs_wen_o     ), 
+        .rd_addr_o      (mem_rd_addr_o      ), 
+        .rd_data_o      (mem_rd_data_o      )
+    );
+
+    //ram
+    ram u_ram(
+        .clk            (clk                ),
+        .addr           (ex_mem_addr_o      ),
+        .data_in        (ex_mem_dout        ),
+        .cs             (ex_mem_cs_o        ),
+        .we             (ex_mem_we_o        ),
+        .wem            (ex_mem_wem_o       ),
+        .mem_data_o     (mem_data_o         )
+    );
+
+    //mem_wb
+    mem_wb u_mem_wb(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .inst_i         (mem_inst_o         ),
+        .instaddr_i     (mem_instaddr_o     ),
+        .regs_wen_i     (mem_regs_wen_o     ),
+        .rd_addr_i      (mem_rd_addr_o      ),
+        .rd_data_i      (mem_rd_data_o      ),
+        .inst_o         (mem_wb_inst_o      ),
+        .instaddr_o     (mem_wb_instaddr_o  ),
+        .regs_wen_o     (mem_wb_regs_wen_o  ),
+        .rd_addr_o      (mem_wb_rd_addr_o   ),
+        .rd_data_o      (mem_wb_rd_data_o   ),
+        .hold_en_i      (hold_en_o          )
+    );
+
+    //wb
+    wb u_wb(
+        .clk            (clk                ),
+        .rstn           (rstn               ),
+        .inst_i         (mem_wb_inst_o      ),
+        .instaddr_i     (mem_wb_instaddr_o  ),
+        .regs_wen_i     (mem_wb_regs_wen_o  ),
+        .rd_addr_i      (mem_wb_rd_addr_o   ),
+        .rd_data_i      (mem_wb_rd_data_o   ),
+        .regs_wen_o     (wb_regs_wen_o      ),
+        .rd_addr_o      (wb_rd_addr_o       ),
+        .rd_data_o      (wb_rd_data_o       )
+    );
+endmodule
